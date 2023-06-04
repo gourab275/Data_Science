07404adeba30c69456bb46a7e00dc747aeb42e77
Gourab Biswakarma <131770948+gourab275@users.noreply.github.com>
2023-06-04 20:30:34 +0530

    Add files via upload

---
+++ Credit Card Fraud Detector/main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from catboost import CatBoostClassifier
+import pickle
+import numpy as np
+
+model =  pickle.load(open('frd.pkl','rb'))
+
+st.title("Credit Card Fraud Detection Model")
+st.write("Enter the following features to check if the transaction is legitimate or fraudulent:")
+
+# create input fields for user to enter feature values
+input_df = st.text_input('Input All features')
+input_df_lst = input_df.split(',')
+# create a button to submit input and get prediction
+submit = st.button("Submit")
+
+if submit:
+    # get input feature values
+    features = np.array(input_df_lst, dtype=np.float64)
+    # make prediction
+    prediction = model.predict(features.reshape(1,-1))
+    # display result
+    if prediction[0] == 0:
+        st.write("Legitimate transaction")
+    else:
+        st.write("Fraudulent transaction")
